657fe119b6929eba7e2597c2d93c27d3dc663b3c
Taran <thetaranneeld@gmail.com>
2019-08-06 15:34:43 -0600

    A few eventListeners are working

--- js/index.js
+++ js/index.js
@@ -1,86 +1,94 @@
 //Header
-let logoHeading = document.querySelector('.logo-heading');
-let navLink = document.querySelectorAll('.nav-link');
+const logoHeading = document.querySelector('.logo-heading');
+const links = document.querySelectorAll('a');
 
 //Top content
-let intro = document.querySelector('.intro');
+const intro = document.querySelector('.intro');
 
 //Top middle content
-let topMiddleContent = document.querySelectorAll('.text-content');
-let middleImages = document.querySelectorAll('.img-content');
+const topMiddleContent = document.querySelectorAll('.text-content');
+const middleImages = document.querySelectorAll('.img-content');
 
 //Bottom middle content
-let bottomImgSection = document.querySelector('.content-destination');
-let bottomImgTitle = document.querySelector('.content-destination h2');
-let bottomImg = document.querySelector('.content-destination img');
+const bottomImgSection = document.querySelector('.content-destination');
+const bottomImgTitle = document.querySelector('.content-destination h2');
+const bottomImg = document.querySelector('.content-destination img');
 
 //Bottom content
-let bottomContent = document.querySelectorAll('.destination');
-let buttons = document.querySelectorAll('.btn');
+const bottomContent = document.querySelectorAll('.destination');
+const buttons = document.querySelectorAll('.btn');
 
 //Footer
-let copyright = document.querySelector('.footer');
+const copyright = document.querySelector('.footer');
 
 
 
 //Event listeners
-logoHeading.addEventListener('load', (event) => {
+window.addEventListener('load', (event) => {
    //Add spin animation on window load
-
+    //TweenMax.to(logoHeading, 2, {rotationY:360});
 });
 
-navLink.addEventListener('mouseover', (event) => {
-    //Enlarge target link
+links.forEach(link => {
+    link.addEventListener('mouseover', (event) => {
+        //Enlarge target link size
+        TweenMax.to(event.target, 0.5, {scale:1.2});
+     });
+     link.addEventListener('mouseleave', (event) => {
+        //Reset target link size
+        TweenMax.to(event.target, 0.5, {scale:1});
+     });
+});
+
+links.forEach(link => {
+    link.addEventListener('click', (event) => {
+        //Stop links from refreshing the page
+        event.preventDefault();
+     });
+});
+
+//  intro.addEventListener('select', (event) => {
+//     //Recolor selected intro elements
+
+//  });
+
+//  topMiddleContent.addEventListener('resize', (event) => {
+//     //Flip text and image positions on resize
+
+//  });
+
+//  middleImages.addEventListener('scroll', (event) => {
+//     //Spin middle images on scroll
+
+//  });
+
+//  bottomImgTitle.addEventListener('mouseover', (event) => {
+//     //Slide to right side on hover
  
- });
+//  });
 
- navLink.addEventListener('click', (event) => {
-    //Stop nav links from refreshing the page
-   
- });
+//  bottomImg.addEventListener('click', (event) => {
+//     //Flip text and image positions on image click
 
- intro.addEventListener('select', (event) => {
-    //Recolor selected intro elements
+//  });
 
- });
-
- topMiddleContent.addEventListener('resize', (event) => {
-    //Flip text and image positions on resize
-
- });
-
- middleImages.addEventListener('scroll', (event) => {
-    //Spin middle images on scroll
-
- });
-
- bottomImgTitle.addEventListener('mouseover', (event) => {
-    //Slide to right side on hover
+//  bottomContent.addEventListener('mouseover', (event) => {
+//     //recolor target element
  
- });
-
- bottomImg.addEventListener('click', (event) => {
-    //Flip text and image positions on image click
-
- });
-
- bottomContent.addEventListener('mouseover', (event) => {
-    //recolor target element
+//  });
  
- });
+//  buttons.addEventListener('dblclick', (event) => {
+//     //Append new child button under target button on double click
+//     //Prevent target button from creating a new child button, if one was already created
  
- buttons.addEventListener('dblclick', (event) => {
-    //Append new child button under target button on double click
-    //Prevent target button from creating a new child button, if one was already created
- 
- });
+//  });
 
- copyright.addEventListener('dragstart', (event) => {
-    //Change text on dragstart
+//  copyright.addEventListener('dragstart', (event) => {
+//     //Change text on dragstart
  
- });
+//  });
 
- copyright.addEventListener('dragend', (event) => {
-    //Reset text and change text color on dragend
+//  copyright.addEventListener('dragend', (event) => {
+//     //Reset text and change text color on dragend
  
- });
+//  });
