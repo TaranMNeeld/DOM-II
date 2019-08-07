528cdfea7975a336eb0ce4780737bad1ad6fa59c
Taran <thetaranneeld@gmail.com>
2019-08-06 19:10:34 -0600

    finished the project

--- js/index.js
+++ js/index.js
@@ -1,24 +1,11 @@
-//Header
+const body = document.querySelector('body');
 const logoHeading = document.querySelector('.logo-heading');
 const links = document.querySelectorAll('a');
-
-//Top content
 const introParagraph = document.querySelector('.intro p');
-
-//Top middle content
 const topMiddleContent = document.querySelector('.content-section');
-const middleImages = document.querySelectorAll('.img-content');
-
-//Bottom middle content
-const bottomImgSection = document.querySelector('.content-destination');
-const bottomImgTitle = document.querySelector('.content-destination h2');
+const bottomImgContent = document.querySelector('.content-destination');
 const bottomImg = document.querySelector('.content-destination img');
-
-//Bottom content
-const bottomContent = document.querySelectorAll('.destination');
 const buttons = document.querySelectorAll('.btn');
-
-//Footer
 const copyright = document.querySelector('.footer');
 
 
@@ -33,45 +20,58 @@ links.forEach(link => {
     link.addEventListener('mouseover', (event) => {
         //Enlarge target link size
         TweenMax.to(event.target, 0.5, {scale:1.2});
-     });
-     link.addEventListener('mouseleave', (event) => {
+    });
+    link.addEventListener('mouseleave', (event) => {
         //Reset target link size
         TweenMax.to(event.target, 0.5, {scale:1});
-     });
+    });
 });
 
 links.forEach(link => {
     link.addEventListener('click', (event) => {
         //Stop links from refreshing the page
         event.preventDefault();
-     });
+    });
 });
 
 //Change intro title when hovering over intro paragraph
- introParagraph.addEventListener('mouseover', (event) => {
+introParagraph.addEventListener('mouseover', (event) => {
     const introTitle = document.querySelector('.intro h2');
     introTitle.textContent = "This Is The Intro!";
- });
-
- //Reset intro title when not hovering over intro paragraph
- introParagraph.addEventListener('mouseleave', (event) => {
-    const introTitle = document.querySelector('.intro h2');
-    introTitle.textContent = "Welcome To Fun Bus!";
- })
-
- window.addEventListener('resize', (event) => {
-    //Flip text and image positions on resize
-    topMiddleContent.style.flexDirection = 'row-reverse';
- });
-
-window.addEventListener('scroll', (event) => {
-    
 });
 
- 
+//Reset intro title when not hovering over intro paragraph
+introParagraph.addEventListener('mouseleave', (event) => {
+    const introTitle = document.querySelector('.intro h2');
+    introTitle.textContent = "Welcome To Fun Bus!";
+})
+
+window.addEventListener('resize', (event) => {
+    //Flip text and image positions on resize
+    topMiddleContent.style.flexDirection = 'row-reverse';
+});
+
+//Enlarge bottom image content on click,
+//but stop from enlarging if clicking on the image
+bottomImgContent.addEventListener('click', (event) => {
+    bottomImg.addEventListener('click', (event) => {
+        event.stopPropagation();
+    });
+    TweenMax.to(bottomImgContent, 0.5, {scale:1.3});
+});
+bottomImgContent.addEventListener('mouseleave', (event) => {
+    TweenMax.to(bottomImgContent, 0.5, {scale:1});
+});
+
+//Change body text color on scroll
+window.addEventListener('scroll', (event) => {
+    body.style.color = 'blue';
+});
+
+//Change button color on double click
 buttons.forEach(button => {
     button.addEventListener('dblclick', (event) => {
-
+        event.target.style.backgroundColor = 'purple';
     });
 });
 
